23dd84d00da90c368f3dc719b57bcc29c5e466cf
agotanemes <nem_agi_96@yahoo.com>
2020-02-26 15:11:23 +0200

    Login with empty email and wrong pass Login with empty email and good pass Login with empty pass and  wrong email Login with empty pass and good email

--- src/main/java/page/LoginPage.java
+++ src/main/java/page/LoginPage.java
@@ -4,7 +4,7 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.WebElement;
-
+//bla
 @DefaultUrl("http://qa2.dev.evozon.com/customer/account/login/")
 public class LoginPage extends AbstractPage {
     @FindBy(id = "email")
